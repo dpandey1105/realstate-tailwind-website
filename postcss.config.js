c6bef76e652286449694aef43df2396df12fefd3
dpandey1105 <dpandey1105>
2024-05-11 00:49:04 +0530

    css build fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
+const tailwindcss = require('tailwindcss');
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-  },
-}
+  plugins: [
+    'postcss-preset-env',
+    tailwindcss
+  ],
+};
